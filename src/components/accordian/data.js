0cf33c02ea2cb2f3ba32562335afaf2f9dd39bae
Divakar Jha <divakarjha9128@gmail.com>
2024-05-10 18:45:03 +0530

    first commit

---
+++ src/components/accordian/data.js
@@ -0,0 +1,28 @@
+const data = [
+  {
+    id:'1',
+    question: "what is your name?",
+    answer:
+    " my name is divakar jha"
+  },
+  {
+    id:'2',
+    question: "can you laugh?",
+    answer:
+    " Yah sure!! HA Ha Ha..."
+  },
+  {
+    id:'3',
+    question: "According to you who is genius",
+    answer:
+    " Ofcourse Divakar Jha is genius."
+  },
+  {
+    id:'4',
+    question: "Who is divakar",
+    answer:
+    " divakar is 23 year young man"
+  },
+];
+
+export default data;
